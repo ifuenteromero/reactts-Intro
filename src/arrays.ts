22cc1f91bf9c508d18c62da6616b7f72b60483af
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-03-24 08:19:18 +0100

    367 arrys in ts

---
+++ src/arrays.ts
@@ -0,0 +1,6 @@
+const carMakers: string[] = [];
+carMakers.push('ford', 'toyota', 'chevy');
+
+const dates: Date[] = [new Date(), new Date()];
+
+const carByMaker: string[][] = [['f150'], ['corolla'], ['']];
