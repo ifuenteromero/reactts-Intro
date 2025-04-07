df7bdf5aa7d77e7933715b05150dcef7410e6f8c
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-04-07 08:03:21 +0200

    Fixing annotations with interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,14 +1,16 @@
-const oldCivic = {
+interface Vehicle {
+	name: string;
+	year: number;
+	broken: boolean;
+}
+
+const oldCivic: Vehicle = {
 	name: 'civic',
 	year: 2000,
 	broken: true,
 };
 
-const printVehicle = (vehicle: {
-	name: string;
-	year: number;
-	broken: boolean;
-}): void => {
+const printVehicle = (vehicle: Vehicle): void => {
 	console.log({
 		name: vehicle.name,
 		year: vehicle.year,
