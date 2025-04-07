f0ec69d19d7e67082bd3e8ce6383cfc8ba0f169e
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-04-07 07:40:51 +0200

    tuplas

---
+++ src/tuples.ts
@@ -0,0 +1,13 @@
+const drink = {
+	color: 'brown',
+	carbonated: true,
+	sugar: 40,
+};
+
+// tupla
+const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// alias
+type Drink = [string, boolean, number];
+
+const sprite: Drink = ['clear', true, 40];
