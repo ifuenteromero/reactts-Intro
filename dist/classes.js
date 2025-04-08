ad0a054eec1dbd6671b9db65630c426865afd4a4
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-04-08 07:45:25 +0200

    Classes

---
+++ dist/classes.js
@@ -0,0 +1,11 @@
+class Vehicle {
+    drive() {
+        console.log('chugga chugga');
+    }
+    honk() {
+        console.log('beep');
+    }
+}
+const vehicle = new Vehicle();
+vehicle.drive();
+vehicle.honk();
